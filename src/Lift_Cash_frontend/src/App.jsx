42f8d2080edf9ec55117f4bfef3ac4224d8556f0
shadab khan <shadabkhan@shadabs-MBP.lan>
2024-10-09 13:07:50 +0530

    hero/about section

--- src/Lift_Cash_frontend/src/App.jsx
+++ src/Lift_Cash_frontend/src/App.jsx
@@ -1,9 +1,11 @@
 import Header from "./components/Header";
+import LandingPage from "./pages/LandingPage";
 
 function App() {
   return (
     <>
       <Header />
+      <LandingPage />
     </>
   );
 }
