0ea3c1f546d94ef2fd7ff8ebd85f3e1aa3ec4289
Rajnish Tripathi <rajnish.tripathi@quadbtech.com>
2024-10-09 15:48:29 +0530

    Internet Identity Canister added

--- deploy.sh
+++
@@ -1,3 +0,0 @@
-cargo build --release --target wasm32-unknown-unknown --package Lift_Cash_backend
-
-candid-extractor target/wasm32-unknown-unknown/release/Lift_Cash_backend.wasm >src/Lift_Cash_backend/Lift_Cash_backend.did
