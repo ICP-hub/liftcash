aa12fbd76c1ec18e0cae81909ea1cf4f4f045bd0
Aditya Gaba <aditya.gaba@quadbtech.com>
2024-10-10 13:12:03 +0530

    Create deploy.sh

---
+++ deploy.sh
@@ -0,0 +1,5 @@
+cargo build --release --target wasm32-unknown-unknown --package Lift_Cash_backend
+
+candid-extractor target/wasm32-unknown-unknown/release/Lift_Cash_backend.wasm >src/Lift_Cash_backend/Lift_Cash_backend.did
+
+dfx deploy 
