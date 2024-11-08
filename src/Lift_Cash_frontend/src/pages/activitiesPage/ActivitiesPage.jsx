09c02e0a2473effec5eb543ae3b58a5ba57346b2
Shadab khan <shadab.khan@quadbtech.com>
2024-11-08 14:09:15 +0530

    Update ActivitiesPage.jsx

--- src/Lift_Cash_frontend/src/pages/activitiesPage/ActivitiesPage.jsx
+++ src/Lift_Cash_frontend/src/pages/activitiesPage/ActivitiesPage.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import DashBoardHead from "../../components/dashboardHead/DashBoardHead";
 import "./ActivitiesPage.css";
 import { survey } from "./constants/Survey";
