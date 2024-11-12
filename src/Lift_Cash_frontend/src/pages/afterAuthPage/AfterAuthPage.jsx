bf804e4247001b417a4acd7fa36c3f1aba479ae7
shadabkhan16 <shadab.khan@quadbtech.com>
2024-11-12 16:22:55 +0530

    Add : Animation

--- src/Lift_Cash_frontend/src/pages/afterAuthPage/AfterAuthPage.jsx
+++ src/Lift_Cash_frontend/src/pages/afterAuthPage/AfterAuthPage.jsx
@@ -1,22 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
 import HeaderAfterAuth from "../../components/headerAfterAuth/HeaderAfterAuth";
 import bgimg from "../../assets/images/background.svg";
+import Waveform from "../../components/Waveform";
 
 const AfterAuthPage = ({ children }) => {
+  const [isAnimationActive, setIsAnimationActive] = useState(false);
+
+  // Toggle function for background animation
+  const onToggleAnimation = () => {
+    setIsAnimationActive((prevState) => !prevState);
+  };
   return (
-    <div className="">
-      <HeaderAfterAuth />
+    <div>
+      <HeaderAfterAuth onToggleAnimation={onToggleAnimation} />
       <div
         style={{
-          backgroundImage: `url(${bgimg})`,
+          backgroundImage: isAnimationActive ? "" : `url(${bgimg})`,
           backgroundRepeat: "no-repeat",
           backgroundPosition: "center center",
           backgroundAttachment: "fixed",
-          
         }}
         className=""
       >
-        <main>{children}</main>
+        {/* Conditionally render Waveform animation */}
+        {isAnimationActive && (
+          <div className="absolute  ">
+            <Waveform />
+          </div>
+        )}
+        <main className="relative ">{children}</main>
       </div>
     </div>
   );
