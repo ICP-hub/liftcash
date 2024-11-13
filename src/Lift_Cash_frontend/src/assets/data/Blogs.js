9eb9159e9f3537131bc0f8bb7cca212158a252ab
vijaykumar1965 <vijay.kumar@quadbtech.com>
2024-11-13 16:21:33 +0530

    feat(frontend svg icon implemented in participationSection and image change in blockSection)

--- src/Lift_Cash_frontend/src/assets/data/Blogs.js
+++ src/Lift_Cash_frontend/src/assets/data/Blogs.js
@@ -3,15 +3,15 @@ export const blogs = [
     title: "Lift Cash Unchained",
     date: "Mon Mar 18 2024",
     image:
-      "https://plus.unsplash.com/premium_photo-1681487464375-7cde580bf4ec?q=80&w=2940&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      "https://miro.medium.com/v2/resize:fit:720/format:webp/1*t7wM5PqOGrxJSPLw9t2GRw.png",
     description: "No content available",
-    link: "https://medium.com/lift-cash/the-value-and-values-of-lift-cash-26817c4ff90b",
+    link: "https://.com/lift-cash/the-value-and-values-of-lift-cash-26817c4ff90b",
   },
   {
     title: "Revolutionising DAO Governance with Fractal Sortition",
     date: "Thu Dec 14 2023",
     image:
-      "https://plus.unsplash.com/premium_photo-1681487464375-7cde580bf4ec?q=80&w=2940&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      "https://miro.medium.com/v2/resize:fit:720/format:webp/1*xD7fnBxITIn_A9FVhFf4gw.png",
     description: "No content available",
     link: "https://medium.com/lift-cash/welcome-to-the-freeos-lift-cash-community-growth-contest-e311f9b93d24",
   },
@@ -20,7 +20,7 @@ export const blogs = [
       "Implementing a Sortition-based DAO for Policymaking and AI Governance",
     date: "Sat Jun 24 2023",
     image:
-      "https://plus.unsplash.com/premium_photo-1681487464375-7cde580bf4ec?q=80&w=2940&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      "https://miro.medium.com/v2/resize:fit:720/format:webp/0*kwAXLrUUKnbuPck0",
     description: "No content available",
     link: "https://medium.com/lift-cash/how-lift-cash-works-the-mechanics-of-our-economic-system-7b1954b5fccd",
   },
